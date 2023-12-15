d9fff6dcabc5f80761dcf3f2459c31f189244cd8
MaxBeaumet <m.beaumet@xefi.fr>
2023-12-15 23:13:09 +0100

    Add some commment and verification of preject

--- ToDoListProject/App.js
+++ ToDoListProject/App.js
@@ -1,101 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { FlatList, View, Text, StyleSheet, TextInput, Button, Alert} from 'react-native';
-import {Title} from 'react-native-paper';
-
-const sampleGoals = [
-  "Faire les courses",
-  "Aller à la salle de sport 3 fois par semaine",
-  "Monter à plus de 5000m d'altitude",
-  "Acheter mon premier appartement",
-  "Perdre 5 kgs",
-  "Gagner en productivité",
-  "Apprendre un nouveau langage",
-  "Faire une mission en freelance",
-  "Organiser un meetup autour de la tech",
-  "Faire un triathlon",
-];
-// L'ajout de nouveau items de la liste 
-// La modification des items de la liste 
-// la suppression d'un item de la liste 
-// Création d'une modale 
-// l'affichage des items de la liste 
-const renderItem = ({ item }) => (
-  <View style={styles.itemContainer}>
-    <Text>{item}</Text>
-    {/* Bouton de suppression */}
-  </View>
-);
-
-const TextInputExample = () => {
-  const [text, onChangeText] = React.useState('Useless Text');
-  const [number, onChangeNumber] = React.useState('');
-
-  return (
-    <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="useless placeholder"
-        keyboardType="numeric"
-      />
-    </SafeAreaView>
-  );
-};
-
-
+import React, { useState } from 'react';
+import { FlatList, View, Text, StyleSheet, TextInput, SafeAreaView, Button, Alert, ImageBackground, TouchableOpacity} from 'react-native';
+import {Modal, Title} from 'react-native-paper';
+import {FontAwesome as Icon} from 'react-native-vector-icons';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
 
-  const handleButtonPress = () => {
-    // Code à exécuter lorsque le bouton est pressé
-    Alert.alert('Bouton pressé !');
-  };
+  // Ajout de l'image de fond 
+  const image = {uri: 'https://cdn.shopify.com/s/files/1/0287/6738/7780/files/22.jpg?v=1619602571'};
+  const [newTask, setNewTask] = useState('');
+  const [valeurInput, setValeurInput] = useState('');
+
+  // Liste des tache qui seront afficher au lancement de l'application
+  const [sampleGoals,setSampleGoals] = useState( [
+    "1.Faire les courses",
+    "2.Aller à la salle de sport 3 fois par semaine",
+    "3.Monter à plus de 5000m d'altitude",
+    "4.Acheter mon premier appartement",
+    "5.Perdre 5 kgs",
+    "6.Gagner en productivité",
+    "7.Apprendre un nouveau langage",
+    "8.Faire une mission en freelance",
+    "9.Organiser un meetup autour de la tech",
+    "10.Faire un triathlon",
+  ]);
+  
+  // Constante qui a pour but de permettre d'afficher dans une les item de la liste
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Text>{item}</Text>
+      {/*  Icone de poubelle comme bouton pour la supression d'élément */}
+      <Icon name="trash" size={30} color="#900"></Icon> 
+    </View> 
+  );
+
   return (
-    <View style={styles.container}>
-      {/* Ajout d'une image de fond */}
-      <Title>To Do List</Title>
-      <FlatList data={sampleGoals} renderItem={renderItem} />
-
-      {/* Affichage en ligne */}
-      <TextInput
-        placeholder="Qu'elle nouvelle tache voulez vous ajoutez ?"
-        // keyboardType="alphabetic"
-        onPress={() => console.log("Test")}
-      />
-      <Button title="Add" onPress={handleButtonPress()}/>
-
-      <StatusBar style="auto" />
-    </View>
+    //  Affichage de l'image de fond 
+    <ImageBackground source={image} resizeMode="cover" style={styles.imageContainer}>
+        <View style={{marginTop:20}}>        
+          <Title>To Do List</Title>
+        </View> 
+          {/* Balise permettant d'afficher les éléments sous forme de liste */}
+          <FlatList data={sampleGoals} renderItem={renderItem} />
+          
+          {/* Affiche sur une ligne une zone de texte et le bouton d'ajout d'une nouvelle task */}
+          <View style={styles.rowContainer}>
+            <TextInput
+                placeholder="Qu'elle nouvelle tache voulez vous ajoutez ?"
+                value={valeurInput}
+                onChangeText={e => setValeurInput(e)}
+                style={styles.input}
+            />
+              <Button
+                title="Add"
+                onPress={() => {
+                  setSampleGoals((goals) => [...goals, sampleGoals.length+1+"."+valeurInput]);
+                }}
+              />
+          </View>
+        <StatusBar style="auto" />
+      </ImageBackground>
   );
 }
-
+// Constante permettant d'ajouter du style sur l'application mpobile 
 const styles = StyleSheet.create({
   container: {
     textAlign:"center",
     marginTop: 50,
   },
-  itemContainer: {
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-    padding: 10,
-  },
   Text: {
     fontSize: 24,
     textAlign: "center",
   },
-
-  input: {
-    height: 40,
-    marginTop: 200,
-    borderWidth: 1,
-    padding: 10,
-    flexDirection: 'row',
+  imageContainer: {
+    flex:1,
   },
-});
+  rowContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      display: 'flex',
+      justifyContent: 'space-between',
+    },
+  });
