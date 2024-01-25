552eb7ae20f4ad5d1b48ae1bbe79dbc358348409
MaxBeaumet <m.beaumet@xefi.fr>
2024-01-25 23:11:25 +0100

    Final version of App 2 Weather React Native

--- WeatherApp/WeatherApp/App.js
+++ WeatherApp/WeatherApp/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, ImageBackground, ScrollView } from 'react-native';
 import * as Location from 'expo-location';
-import axios from 'axios';
+// import momant from 'momant';
 
 export default function App() {
+
   // const axios = require(axios);
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
@@ -13,17 +14,17 @@ export default function App() {
   const [dataResponseTemp,setDataResponseTemp] = useState(null);
   const [dataResponseDescription,setDataDescription] = useState(null);
   const [DataIconWeather,setDataIconWeather] = useState(null);
-  const [list, setList] = useState([]);
-  const [DataResponseTempPast,setDataResponseTempPast] = useState(null);
   const [weatherData, setWeatherData] = useState(null);
+  const [DataResponseTempPast, setDataResponseTempPast] = useState(null);
 
 
   const [dataResponseNamePast,setDataResponseNamePast] = useState(null);
-  const [dataList,setDataList] = useState(null);
-  const [dataWeatherTempPre,setDataWeatherTempPre]= useState(null);
-  const [ForecastData,setForecastData]=useState([]);
-  const apiKey = `86be9fd2c35ac6c7cdfdef1ca030a3f0`
+  // const [dataList,setDataList] = useState(null);
+  // const [dataWeatherTempPre,setDataWeatherTempPre]= useState(null);
+  // const [ForecastData,setForecastData]=useState([]);
+  const apiKey = `2f071855ca5ff35e7177e187257888e3`
 
+  // Récupération de la localisation de la personne 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -39,34 +40,42 @@ export default function App() {
     })();
   }, []);
 
+// Récupération des données météo en direct 
 useEffect(() => {
-  if (latitude != null && longitude != null){
-    // Mettre un loader ici 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang={fr}&units=metric&appid=${apiKey}`)
-    .then (response =>  response.json())
-    .then(response => {console.log(response);return response})
-    .then (response => {setDataResponseName(response.name); return (response)})
-    .then (response => {setDataResponseTemp(response.main.temp); return (response)})
-    .then(response => {setDataDescription(response.weather[0].description); return(response)})
-    .then(response => {setDataIconWeather(response.weather[0].icon); return(response)})
+  if (latitude != null && longitude != null) { // Si la latitude et la longitude sont différentes de null 
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&lang=fr&units=metric&appid=${apiKey}`)
+      .then(response => response.json())
+      .then(data => {
+        setDataResponseName(data.name);
+        setDataResponseTemp(data.main.temp);
+        setDataDescription(data.weather[0].description);
+        setDataIconWeather(data.weather[0].icon);
+      })
+      .catch(error => {
+        console.error("Erreur lors de la récupération des données météorologiques:", error);
+      });
   }
+}, [latitude, longitude, apiKey]);
 
-}, [latitude,longitude])
 
-// Prévisionnel : 
+    
+// }, [latitude,longitude])
+
+// Prévisionnel : Valeur prévisionnel de la météo avec l'utilisationn de Forecast
 function fetchForecast(lat, lon) {
   const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=fr`;
 
   axios.get(forecastUrl)
     .then(function(response) {
-      console.log(response.response)
-      setForecastData(response.config.main);     
+      setForecastData(response.config.main);   
+      setDataResponseNamePast(response.name);
+      setDataResponseTempPast(response.main.temp);        
     })
     .catch(function(error) {
       console.error('Erreur lors de la récupération des données de prévision:', error);
     });
 }
-  // console.log(fetchForecast("45.750000","4.850000"));
 
   let text = 'Waiting..';
   if (errorMsg) {
@@ -75,45 +84,54 @@ function fetchForecast(lat, lon) {
     text = JSON.stringify(location);
   }
 
+  // Style de l'application 
+  const styles = StyleSheet.create({
+    header: {
+      backgroundColor: '#AEF6F4',
+      padding: 15,
+      alignItems: 'center',
+      size:15,
+      fontWeight:'bold',
+    },
+    title: {
+      marginTop:15,
+    },
+    value:{
+      marginRight:10,
+    },
+    scrollView: {
+      marginHorizontal: 20,
+    },
+    preview: {
+      flexDirection:'row',
+      justifyContent: 'space-around',
+      marginRight:10,
+    }
 
-  if ({DataIconWeather} != null){
-    // console.log(dataResponseNamePast)
-    return (
-      
-      <View style={styles.container}>
-        <Text style={styles.title}>Weather App</Text>
+  })
 
-        <Text style={styles.value}>{dataResponseName}</Text>
-        <Text>{dataResponseTemp} ° C</Text>
-        <Text>{dataResponseDescription}</Text>
-        <Image style={styles.iconWeather} source={{uri: `http://openweathermap.org/img/wn/${DataIconWeather}.png`}}></Image>
-      
-        <Text>Prévisionel :</Text>
-        <Text style={styles.value}>{dataResponseNamePast}</Text>
-        {/* <Text>{DataResponseTempPast}</Text> */}
-        {/* <Text>{weatherData.name}</Text> */}
-      {/* <Text>{weatherData.main.temp}°C</Text> */}
-      
-      </View>
-      
-    );
-  }
-
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 18,
-    textAlign: 'center',
-  },
-  iconWeather: {
-    width:100,
-    height:100,
-  }
-})
+ return (
+  <View>
+    <View style={styles.header}>
+      <Text style={styles.title}>Weather App</Text>
+    </View>
+    <View style={styles.container}>
+      <Text>{dataResponseName}</Text>
+      <Text>{dataResponseTemp} ° C</Text>
+      <Text>{dataResponseDescription}</Text>
+      <Image source={{uri: `http://openweathermap.org/img/wn/${DataIconWeather}.png`}}></Image>
+    </View>
+    <View>
+      <Text>Météo Actuel :</Text>
+      <Text style={styles.value}>{dataResponseName}</Text>
+      <Text>{dataResponseTemp} ° C</Text>
+      <Text>{dataResponseDescription}</Text>
+    </View>
+    <View>
+    <Text>Prévisionel :</Text>
+      <Text style={styles.value}>{dataResponseNamePast}</Text>
+      <Text>{DataResponseTempPast}</Text>
+      <Text>{weatherData.name}</Text>
+    </View>
+</View>
+)}
